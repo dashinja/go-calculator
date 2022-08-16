83a3bb5590ae1f8b28ec2cc9e87cafbe1100704b
Byron <byron.miller@gmail.com>
2022-08-16 03:01:29 -0400

    Finish out precalc

--- calculator.go
+++ calculator.go
@@ -1,18 +1,43 @@
 package main
 
 import (
-	"fmt"
-	// "github.com/dashinja/calculator/math/precalc"
 	"calculator/math/precalc"
+	"fmt"
 )
 
-var testRange = []int{1,2,3,4,5,6,7,8,9,10}
+var testInputs = []int{1,2,8,4,5,6,9,8,9,10}
 
-func main() {
-  calculator()
+type Inputs struct{
+  testRange []int
+  answer int
+  err bool
 }
 
-func calculator() {
-  fmt.Println("Run Calculator")
-  fmt.Printf("Output of %d", precalc.Add(testRange))
+func main() {
+  inputs := Inputs{testRange: testInputs}
+  inputs.calculator("add")
+  inputs.calculator("sub")
+  inputs.calculator("mult")
+  inputs.calculator("div")
+  inputs.calculator("sq")
+}
+
+func (I *Inputs) calculator(operation string) {
+  switch operation {
+  case "add":
+    I.answer = precalc.Add(I.testRange)
+   case "sub": 
+   I.answer = precalc.Sub(I.testRange) 
+  case "mult":
+    I.answer = precalc.Mult(I.testRange)
+  case "div":
+    I.answer = precalc.Div(I.testRange)
+  default:
+    I.err = true
+    fmt.Println("Invalid operation")
+  }
+  
+ if (!I.err) {
+   fmt.Printf("Answer: %d\n", I.answer)
+ }
 }
--- math/precalc/precalc.go
+++ math/precalc/precalc.go
@@ -1,19 +1,35 @@
 package precalc
 
-import (
-	"fmt"
-)
-
 var result int
-
-func precalc() {
-	fmt.Println("I'm precalc")
-}
 
 func Add(nums []int) int {
 	result = 0
 	for _, v := range nums {
 		result += v
+	}
+	return result
+}
+
+func Sub(nums []int) int {
+	result = nums[0]
+	for _, v := range nums {
+		result -= v
+	}
+	return result
+}
+
+func Mult(nums []int) int {
+	result = 1
+	for _, v := range nums {
+		result *= v
+	}
+	return result
+}
+
+func Div(nums []int) int {
+	result = nums[0]
+	for _, v := range nums {
+		result /= v
 	}
 	return result
 }
